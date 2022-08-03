d1174c4b0ac52b53becdc1c5a17fc0c40328f312
Christian Pfaff <madadaskar@googlemail.com>
2022-08-03 12:48:23 +0200

    reworked website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { AppComponent } from './app.component';
-import { DataProtectionComponent } from './data-protection/data-protection.component';
-import { HeaderComponent } from './header/header.component';
-import { ImprintComponent } from './imprint/imprint.component';
-import { ProjectComponent } from './project/project.component';
 
-const routes: Routes = [
-  /* { path: '', component: HeaderComponent },
-  { path: 'project', component: ProjectComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'imprint', component: ImprintComponent },
-  { path: 'data-protection', component: DataProtectionComponent } */
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
