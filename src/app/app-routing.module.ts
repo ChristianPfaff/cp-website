841d268d520d25a4a11e25cb4584341471368cc1
Christian Pfaff <madadaskar@googlemail.com>
2022-08-03 13:00:25 +0200

    reworked website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RouterModule },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'data-protection', component: DataProtectionComponent }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
