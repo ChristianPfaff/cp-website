345c2adc0f5ee32a2f67542769afd4d359e71832
Christian Pfaff <madadaskar@googlemail.com>
2022-08-04 21:21:16 +0200

    reworked website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
 import { ImprintComponent } from './imprint/imprint.component';
-import { ProjectComponent } from './project/project.component';
 import { WebsiteComponent } from './website/website.component';
-import { TalkbusinessComponent } from './talkbusiness/talkbusiness.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: WebsiteComponent },
-  { path: 'website', component: WebsiteComponent },
-  { path: 'project', component: ProjectComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'talkbusiness', component: TalkbusinessComponent},
-  { path: 'imprint', component: ImprintComponent },
+  { path: '', component: WebsiteComponent },  
+  { path: 'imprint', component: ImprintComponent },  
   { path: 'data-protection', component: DataProtectionComponent }  
 ];
 
