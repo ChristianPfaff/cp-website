9f89da4eb64bf106eaedf7e89e891794176001e7
Christian Pfaff <madadaskar@googlemail.com>
2022-08-08 15:11:54 +0200

    Angualar Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
 import { WebsiteComponent } from './website/website.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { WebsiteComponent } from './website/website.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
